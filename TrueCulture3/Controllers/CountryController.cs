7109c71c11c0fe8ba078335f4429b19d14c68247
DannyX424 <dee_mo16@yahoo.com>
2021-05-15 18:33:50 -0700

    Updated Country tab

---
+++ TrueCulture3/Controllers/CountryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TrueCulture3.Models;
+
+namespace TrueCulture3.Controllers
+{
+    public class CountryController : Controller
+    {
+        public IActionResult ListCountries()
+        {
+            List<Countries> worldCountries = new List<Countries>();
+            worldCountries.Add(new Countries { countryName = "Guatemala", countryLocation = "Central America" });
+            worldCountries.Add(new Countries { countryName = "Mexico", countryLocation = "North America" });
+            worldCountries.Add(new Countries { countryName = "Peru", countryLocation = "South America" });
+
+            return View(worldCountries);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TrueCulture3/Models/Countries.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrueCulture3.Models
+{
+    public class Countries
+    {
+        public string countryName { get; set; }
+        public string countryLocation { get; set; }
+    }
+}
